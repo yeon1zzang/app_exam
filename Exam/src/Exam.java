5fbf63518be760d210bb97affb06b95dfb8c3d6e
501-11-PC\501-11 <gusl5449@gmail.com>
2018-02-21 11:16:11 +0900

    180221(2)

--- Exam/src/Exam.java
+++ Exam/src/Exam.java
@@ -3,21 +3,38 @@ import java.awt.*;
 import java.awt.event.WindowAdapter;
 import java.awt.event.WindowEvent;
 import java.awt.event.WindowListener;
+import java.util.Scanner;
 
 public class Exam {
     public static void main(String[] args){
+
+        //정상적으로 처리가 되면 try안에 구문이 처리되고 나눗셈이 정상적으로 수행되었다는 출력이 나옴
+
         try{
-            int num1 = 10;
-            int num2 = 0;
+            Scanner keyboard = new Scanner(System.in);
 
-            double result = num1/num2;
-            System.out.println("결과 : " + result);
-        } catch (ArithmeticException e){
-            System.out.println("안내 : 에러가 발생하였습니다.");
-            System.out.println(e.getMessage());
-            e.printStackTrace();
+            System.out.println("피제수 입력 : ");
+            int num1 = keyboard.nextInt();
+            System.out.println("제수 입력 : ");
+            int num2 = keyboard.nextInt();
+            System.out.println("연산 결과를 저장할 배열의 인덱스 입력 : ");
+            int idx = keyboard.nextInt();
+
+            arr[idx]=num1/num2;
+            System.out.println("나눗셈 결과는 "+arr[idx]);
+            System.out.println("저장된 위치의 인덱스는 "+idx);
+    }
+
+    catch (ArithmeticException e){
+            System.out.println("제수는 0이 될 수 없습니다.");
+            i-=1;
+            continue;
+    }
+
+    catch (ArrayIndexOutOfBoundsException e){
+            System.out.println("유효하지 않은 인덱스 값입니다.");
+            i-=1;
+            continue;
         }
-
-        System.out.println("나눗셈이 정상적으로 수행되었습니다.");
     }
 }

ba034f06e2e7ba57bd337cd5932d916412cea94d
501-11-PC\501-11 <gusl5449@gmail.com>
2018-02-21 14:26:26 +0900

    180221(4)

--- Exam/src/Exam.java
+++ Exam/src/Exam.java
@@ -1,29 +1,8 @@
-import com.sun.tools.attach.AgentLoadException;
-
-import javax.swing.*;
-import java.awt.*;
-import java.awt.event.WindowAdapter;
-import java.awt.event.WindowEvent;
-import java.awt.event.WindowListener;
-import java.util.Scanner;
-
 public class Exam {
-    public static void main(String[] args){
-        try {
-            System.out.println("나이를 입력하세요.");
-            int age = kb.nextInt();
-            System.out.println("당신의 나이는 "+age);
-        } catch (AgenInputException e){
-            System.out.println("");
-            e.printStackTrace();
-        }
-    }
-    public static int readAge() {
-        Scanner kb = new Scanner(System.in);
-        int age = kb.nextInt();
-        if(age <= 0) {
-            AgeInputException ageInputException = new AgeInputException();
-
-        }
+    public static void main(String args){
+        Man bm = new Man("희연");
+        System.out.println(bm.getName());
+        bm.setName("슬기찡");
+        System.out.println(bm.getName());
     }
 }
--- Exam/src/Man.java
+++ Exam/src/Man.java
@@ -3,7 +3,10 @@ class Man{
     public Man(String name) {
         this.name = name;
     }
-    public void tellYourName(){
-        System.out.println("My name is " + name);
+    public void setName(String name){
+        this.name = name;
+    }
+    public String getName(){
+        return name;
     }
 }
